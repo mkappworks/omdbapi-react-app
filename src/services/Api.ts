7c94c045db4fd4ada565c6ad9dc1178ab77376be
Malith Kuruppu <info.mkappworks@gmail.com>
2021-12-22 20:44:31 +0530

    Update Api.ts

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const BaseApi = axios.create({
-  baseURL: "http://www.omdbapi.com",
+  baseURL: "https://www.omdbapi.com",
   headers: {
-    "Content-type": "application/json",
-    "Access-Control-Allow-Methods": "GET",
-    "Access-Control-Allow-Origin": "http://www.omdbapi.com",
+    "Content-type": "application/json"
   },
 });
 
